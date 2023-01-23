1a2095d15157bbf87efc566435c1272aa7263f48
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-23 12:43:46 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/data/data/com.termux/files/usr/bin/bash
+BETTY_STYLE="betty-style"
+BETTY_DOC="betty-doc"
+BETTY_WRAPPER="betty"
+
+APP_PATH="/data/data/com.termux/files/usr/opt/betty"
+BIN_PATH="/data/data/com.termux/files/usr/bin/"
+MAN_PATH="/data/data/com.termux/files/usr/share/man"
+
+echo -e "Installing binaries.."
+
+mkdir -p "${APP_PATH}"
+
+cp "${BETTY_STYLE}.pl" "${APP_PATH}/${BETTY_STYLE}"
+cp "${BETTY_DOC}.pl" "${APP_PATH}/${BETTY_DOC}"
+cp "${BETTY_WRAPPER}.sh" "${APP_PATH}/${BETTY_WRAPPER}"
+
+chmod +x "${APP_PATH}/${BETTY_STYLE}"
+chmod +x "${APP_PATH}/${BETTY_DOC}"
+chmod +x "${APP_PATH}/${BETTY_WRAPPER}"
+
+ln -s "${APP_PATH}/${BETTY_STYLE}" "${BIN_PATH}/${BETTY_STYLE}"
+ln -s "${APP_PATH}/${BETTY_DOC}" "${BIN_PATH}/${BETTY_DOC}"
+ln -s "${APP_PATH}/${BETTY_WRAPPER}" "${BIN_PATH}/${BETTY_WRAPPER}"
+
+echo -e "Installing man pages.."
+
+mkdir -p "${MAN_PATH}"
+
+cp "man/betty.1" "${MAN_PATH}"
+cp "man/${BETTY_STYLE}.1" "${MAN_PATH}"
+cp "man/${BETTY_DOC}.1" "${MAN_PATH}"
+
+echo -e "Updating man database.."
+
+mandb
+
+echo -e "All set."
